5b9be5cd9a2c9330fa78fd02af94d2c0626cd573
MahdiyDev <mahdiydev@gmail.com>
2021-12-22 20:24:50 +0500

    main cjhange

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { db } from './Config/orm.config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*'
+  })
   await app.listen(db.port);
 }
 bootstrap();
